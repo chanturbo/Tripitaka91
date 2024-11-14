1fca8428bf449c1a5ce7458655184712b11dc586
chanturbo <chanturbo@hotmail.com>
2024-11-14 07:31:07 +0700

    main-edit-20241114-07-31

--- lib/widget/bookshow/show_title_sub.dart
+++ lib/widget/bookshow/show_title_sub.dart
@@ -12,6 +12,7 @@ import 'package:tripitaka91/utils/play_audio/audio_manager.dart';
 import 'package:tripitaka91/utils/shared_preferences/shared_user.dart';
 import 'package:tripitaka91/utils/text_title_replace/text_title_replace.dart';
 import 'package:tripitaka91/utils/volume_helper/volume_helper.dart';
+import 'package:tripitaka91/widget/audio/edit_speak.dart';
 import 'package:tripitaka91/widget/auto_text/auto_text.dart';
 import 'package:tripitaka91/widget/login/loading_dialog.dart';
 import 'package:tripitaka91/widget/pageviews/pageviews_html.dart';
@@ -552,6 +553,45 @@ class _ShowTitlePagesState extends State<ShowTitlePages> {
                                   },
                                   child: Icon(
                                     Icons.edit,
+                                    size: widget.isM ? 21 : 16,
+                                    color: Colors.blue[300],
+                                  ),
+                                )
+                              : const SizedBox.shrink(),
+                          (usersChk != null)
+                              ? const SizedBox(width: 10)
+                              : const SizedBox.shrink(),
+                          (usersChk != null)
+                              ? InkWell(
+                                  onTap: () async {
+                                    showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return AlertDialog(
+                                          contentPadding:
+                                              const EdgeInsets.all(5),
+                                          title:
+                                              const Text('แจ้งการอ่านออกเสียง'),
+                                          content: const SizedBox(
+                                            width: double.maxFinite,
+                                            child: EditSpeakScreen(
+                                              comments: '-',
+                                            ),
+                                          ),
+                                          actions: <Widget>[
+                                            TextButton(
+                                              onPressed: () {
+                                                Navigator.of(context).pop();
+                                              },
+                                              child: const Text('ปิดหน้าจอ'),
+                                            ),
+                                          ],
+                                        );
+                                      },
+                                    );
+                                  },
+                                  child: Icon(
+                                    Icons.edit_document,
                                     size: widget.isM ? 21 : 16,
                                     color: Colors.blue[300],
                                   ),
