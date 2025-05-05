3ef4f95ced70b3d1117bd29341c9b0e591d8821f
chanturbo <chanturbo@gmail.com>
2025-05-05 17:32:56 +0700

    main-edit-20250505-17-32

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart'; // ✅ เพิ่มตรงนี้
 import 'package:tripitaka91/utils/theme/theme.dart';
 import 'package:tripitaka91/widget/my_home_page.dart';
 
@@ -15,8 +16,8 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   bool _isGrayscale = false;
+  bool _isLoading = true; // ✅ ใช้โหลด SharedPreferences
 
-  // กรองสีขาว-ดำ
   final grayscaleFilter = const ColorFilter.matrix(<double>[
     0.2126,
     0.7152,
@@ -41,7 +42,39 @@ class _MyAppState extends State<MyApp> {
   ]);
 
   @override
+  void initState() {
+    super.initState();
+    _loadGrayscaleSetting();
+  }
+
+  Future<void> _loadGrayscaleSetting() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _isGrayscale =
+          prefs.getBool('isGrayscale') ?? false; // default: ปิดโหมดขาวดำ
+      _isLoading = false;
+    });
+  }
+
+  Future<void> _toggleGrayscale() async {
+    setState(() {
+      _isGrayscale = !_isGrayscale;
+    });
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isGrayscale', _isGrayscale);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (_isLoading) {
+      // ✅ รอโหลดค่า SharedPreferences ก่อน
+      return const MaterialApp(
+        home: Scaffold(
+          body: Center(child: CircularProgressIndicator()),
+        ),
+      );
+    }
+
     return ColorFiltered(
       colorFilter: _isGrayscale
           ? grayscaleFilter
@@ -53,11 +86,7 @@ class _MyAppState extends State<MyApp> {
         darkTheme: TAppTheme.darkTheme,
         debugShowCheckedModeBanner: false,
         home: MyHomePage(
-          onToggleGrayscale: () {
-            setState(() {
-              _isGrayscale = !_isGrayscale;
-            });
-          },
+          onToggleGrayscale: _toggleGrayscale,
           isGrayscale: _isGrayscale,
         ),
       ),
