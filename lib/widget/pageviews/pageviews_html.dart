6877f1b6c144fd7bea96ff929ca011ebf0de2cb0
chanturbo <chanturbo@gmail.com>
2025-06-06 07:44:29 +0700

    iosx-20250606-07-44

--- lib/widget/pageviews/pageviews_html.dart
+++ lib/widget/pageviews/pageviews_html.dart
@@ -1701,6 +1701,7 @@ class _Tri91PageViewHtmlState extends State<Tri91PageViewHtml> {
                                 text: 'สารบัญหัวข้อธรรม'),
                             onPressed: () {
                               _showDialogTitle(context, true);
+                              _scrollToSelectedIndex1();
                             },
                           ),
                         )
